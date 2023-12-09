675f216cf19902448cd28cb283f5ebd022fa41d0
1zc <liamzcharles@gmail.com>
2023-12-10 03:41:10 +1100

    Root command to identify hookable triggers

--- src/ST-Commands/PlayerCommands.cs
+++ src/ST-Commands/PlayerCommands.cs
@@ -1,6 +1,8 @@
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
 using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Admin;
+using CounterStrikeSharp.API;
 
 namespace SurfTimer;
 
@@ -16,6 +18,28 @@ public partial class SurfTimer
 
         // To-do: players[userid].Timer.Reset() -> teleport player
         playerList[player.UserId ?? 0].Timer.Reset();
+        // player.Teleport(Map.StartZoneOrigin, 0, 0);
+        return;
+    }
+
+    // 
+    [ConsoleCommand("css_triggers", "List triggers eligible for hooking.")]
+    [RequiresPermissions("@css/root")]
+    [CommandHelper(whoCanExecute: CommandUsage.CLIENT_ONLY)]
+    public void Triggers(CCSPlayerController? player, CommandInfo command)
+    {
+        if (player == null)
+            return;
+
+        IEnumerable<CBaseEntity> triggers = Utilities.FindAllEntitiesByDesignerName<CBaseEntity>("trigger_multiple");
+        player.PrintToChat($"Count of triggers: {triggers.Count()}");
+        foreach (CBaseEntity trigger in triggers)
+        {
+            if (trigger.Entity!.Name != null)
+            {
+                player.PrintToChat($"Trigger -> Origin: {trigger.AbsOrigin}, Name: {trigger.Entity!.Name}");
+            }
+        }
         return;
     }
 }
