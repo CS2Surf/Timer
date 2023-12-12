4a162d8359cf4388821e0f7e56b8a92407a4d3b1
Suomipoika <47477423+Suomipoika@users.noreply.github.com>
2023-12-12 13:06:29 +0200

    Async/Await Best Practice to avoid deadlocks, error handling, _connString readonly

--- src/ST-DB/DB.cs
+++ src/ST-DB/DB.cs
@@ -1,24 +1,26 @@
 namespace SurfTimer;
 
+using System;
 using System.Runtime.CompilerServices;
 using CounterStrikeSharp.API;
 using MySqlConnector; // https://dev.mysql.com/doc/connector-net/en/connector-net-connections-string.html
 
 // This will have functions for DB access and query sending
-internal class TimerDatabase 
+internal class TimerDatabase
 {
     private MySqlConnection? _db;
-    private string _connString;
+    private readonly string _connString = string.Empty;
 
     public TimerDatabase()
     {
         // Null'd
     }
 
+
     public TimerDatabase(string host, string database, string user, string password, int port, int timeout)
     {
         this._connString = $"server={host};user={user};password={password};database={database};port={port};connect timeout={timeout};";
-        this._db = new MySqlConnection(this._connString);
+        this._db = new MySqlConnection(_connString);
         this._db.Open();
     }
 
@@ -30,17 +32,49 @@ internal class TimerDatabase
 
     public async Task<MySqlDataReader> Query(string query)
     {
-        MySqlCommand cmd = new MySqlCommand(query, this._db);
-        MySqlDataReader reader = await cmd.ExecuteReaderAsync();
+        return await Task.Run(async () =>
+        {
+            try
+            {
+                if (this._db == null)
+                {
+                    throw new InvalidOperationException("Database connection is not open.");
+                }
 
-        return Task.FromResult(reader).Result;
+                MySqlCommand cmd = new(query, this._db);
+                MySqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+                return reader;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error executing query: {ex.Message}");
+                throw;
+            }
+        });
     }
 
     public async Task<int> Write(string query)
     {
-        MySqlCommand cmd = new MySqlCommand(query, this._db);
-        int rowsAffected = await cmd.ExecuteNonQueryAsync();
+        return await Task.Run(async () =>
+        {
+            try
+            {
+                if (this._db == null)
+                {
+                    throw new InvalidOperationException("Database connection is not open.");
+                }
 
-        return rowsAffected;
+                MySqlCommand cmd = new(query, this._db);
+                int rowsAffected = await cmd.ExecuteNonQueryAsync();
+
+                return rowsAffected;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error executing write operation: {ex.Message}");
+                throw;
+            }
+        });
     }
 }
